54142ee7583e2a042bbfc2ee3df0bda849c77d8f
Ivan Ramos <ivan1900@gmail.com>
2021-12-26 18:19:36 +0100

    fix show date

--- pages/index.js
+++ pages/index.js
@@ -28,13 +28,13 @@ export default function Home({allPostsData}) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({id, data, title}) => (
+          {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
               {title}
               <br />
               {id}
               <br />
-              {data}
+              {date}
             </li>
           ))}
         </ul>
